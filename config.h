7721059acaa6df82e471876706fbbb4154a17243
ChrisMoller <moller@mollerware.com>
2020-08-10 14:25:37 -0400

    all the stuff for ./configure & make w/o ./autogen.sh

---
+++ config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* The prefix for our gettext translation domains. */
+#define GETTEXT_PACKAGE "gtk30"
+
+/* Name of package */
+#define PACKAGE "aplvis"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "moller@mollerware.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "aplvis"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "aplvis 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "aplvis"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
